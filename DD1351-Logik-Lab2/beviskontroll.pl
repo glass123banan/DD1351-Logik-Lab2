5db297a3cf8e12e497c7bcb403485cb34631137a
glass123banan <glass123banan@gmail.com>
2024-11-13 11:48:07 +0100

    adding valid_proof

--- DD1351-Logik-Lab2/beviskontroll.pl
+++ DD1351-Logik-Lab2/beviskontroll.pl
@@ -4,69 +4,20 @@ neg(P)      % !p
 and(P, Q)   % p n q
 or(P, Q)    % p v q
 
-% regelappliceringar
-% and introduction -> om X och Y sant så XnY sant
-andint(X, Y) :-
-    call(X),
-    call(Y).
+% check if step belongs to prems
+valid_line(Prems, Goal, Step) :-
+    member(Step, Prems). 
 
-% and elimination 1
-andel1(X) :-
-    call(and(X, Y)).
+% check some rule
+valid_line(Prems, Goal, Step) :-
+    % todo
 
-% and elimination 2
-andel2(X) :-
-    call(and(Y, X)).
-
-% or introduction 1
-orint(X, Y) :-
-    call(X).
-
-% or introduction 2
-orint(X, Y) :-
-    call(Y).
-
-% or elimination
-orel()
-
-% implication introduction
-impint()
-
-% implication elimination
-impel(Y) :-
-    call(imp(X, Y)),
-    call(X).
-
-% negation introduction
-negint()
-
-% negation elimination
-negel()
-
-% falsum rule
-contel()
-
-% double negation elimination -> !!p = p
-negnegel()
-
-% double negation introduction -> p = !!p
-negnegint()
-
-% MT rule -> p->q, !q => !p
-mt()
-
-% PBC rule -> !p ... cont => p
-pbc()
-
-% LEM rule -> p v !p
-lem()
 
 
 % valid proof check
-valid_proof(Prems, Goal, Proof) :-
-    % TODO : use apply_rule
-
-% apply_rule predicate for each rule made
+valid_proof(Prems, Goal, [Step | RestOfProof]) :-
+    valid_line(Prems, Goal, Step),
+    valid_proof(Prems, Goal, RestOfProof).
 
 % verify -> read file
 verify(InputFileName) :- 
