83eddd583595b86ecf9be36d15fd1df6620c7911
glass123banan <glass123banan@gmail.com>
2024-11-14 12:10:41 +0100

    nobox rules

--- DD1351-Logik-Lab2/beviskontroll.pl
+++ DD1351-Logik-Lab2/beviskontroll.pl
@@ -1,70 +1,96 @@
-% [LineNr, Expr, Rule]
-% check if step belongs to prems
-valid_line(Prems, _, [LineNr, Expr, Premise], _) :-
-    member(Expr, Prems),
-    write('line prems: '),
-    write(LineNr), nl.
-
-% check if line belongs to goal 
-valid_line(_, Goal, [_, Expr, _], _) :-
-    Expr = Goal.
+% propositions: implication, negation, and, or
+imp(P, Q).   % p -> q
+neg(P).      % !p
+and(P, Q).   % p n q
+or(P, Q).    % p v q
 
 % valid_proof calls on helper func w validated steps
 valid_proof(Prems, Goal, ProofSteps) :-
     valid_proof_validator(Prems, Goal, ProofSteps, []).
 
-% basfall
+% basecase: check if Goal is present in ValidatedSoFar
 valid_proof_validator(_, Goal, [], ValidatedSoFar) :-
-    write('hello'), nl,
-    member([_,Goal,_], ValidatedSoFar), 
-    write('basfall 2'), nl.
-
+    member([_,Goal,_], ValidatedSoFar).
 
 % helper func that stores checked lines in ValidatedSoFar
 valid_proof_validator(Prems, Goal, [Step | RestOfProof], ValidatedSoFar) :-
     valid_line(Prems, Goal, Step, ValidatedSoFar), 
     valid_proof_validator(Prems, Goal, RestOfProof, [Step | ValidatedSoFar]).
 
-% propositioner
-imp(P, Q).    % p -> q
-neg(P).      % !p
-and(P, Q).   % p n q
-or(P, Q).    % p v q
+% Check each individual line if its valid
+% Format per proof line: [LineNr, Expr, Rule]
+% check if step belongs to prems
+valid_line(Prems, _, [LineNr, Expr, Premise], _) :-
+    member(Expr, Prems).
 
-% Implication Introduction (→I)
+% Implication Introduction (→I) -> need box
 
 % Implication Elimination (Modus Ponens, →E)
 valid_line(_, _, [LineNr, ImpelExpr, impel(X, Y)], ValidatedSoFar) :-
-    % write('1'),
-    member([X, Antecedent, _], ValidatedSoFar),
-    % write('2'),
-    member([Y, imp(Antecedent, Conclusion), _], ValidatedSoFar),
-    % write('3'),
-    Conclusion = ImpelExpr,
-    write('impel line: '),
-    write(LineNr), nl.
-
-% valid proof check recursive case
+    member([X, Antecedent, _], ValidatedSoFar),                         % P is true
+    member([Y, imp(Antecedent, Conclusion), _], ValidatedSoFar),        % P->Q is true
+    Conclusion = ImpelExpr.                                             % Q is thus true
 
 % Conjunction Introduction (∧I)
+valid_line(_, _, [LineNr, AndExpr, andint(X, Y)], ValidatedSoFar) :-
+    member([X, Conjunct1, _], ValidatedSoFar),
+    member([Y, Conjunct2, _], ValidatedSoFar), 
+    AndExpr = and(Conjunct1, Conjunct2).
 
 % Conjunction Elimination Left (∧E₁)
+valid_line(_,_,[LineNr, AndExpr, andel1(X)], ValidatedSoFar) :-
+    member([X, and(Conjunct1, Conjunct2), _], ValidatedSoFar),
+    AndExpr = Conjunct1.
 
 % Conjunction Elimination Right (∧E₂)
+valid_line(_,_,[LineNr, AndExpr, andel2(X)], ValidatedSoFar) :-
+    member([X, and(Conjunct1, Conjunct2), _], ValidatedSoFar),
+    AndExpr = Conjunct2.
 
 % Disjunction Introduction Left (∨I₁)
+valid_line(_,_,[LineNr, OrExpr, orint1(X)], ValidatedSoFar) :-
+    member([X, Disjunct1, _], ValidatedSoFar),
+    OrExpr = or(Disjunct1, Disjunct2).
 
 % Disjunction Introduction Right (∨I₂)
+valid_line(_,_,[LineNr, OrExpr, orint1(X)], ValidatedSoFar) :-
+    member([X, Disjunct2, _], ValidatedSoFar),
+    OrExpr = or(Disjunct1, Disjunct2).
 
-% Disjunction Elimination (∨E)
+% Disjunction Elimination (∨E) -> need box
 
-% Negation Introduction (¬I)
+% Negation Introduction (¬I) -> need box
 
 % Negation Elimination (¬E)
+valid_line(_,_,[LineNr, NegExpr, negel(X, Y)], ValidatedSoFar) :-
+    member([X, Negand, _], ValidatedSoFar),
+    member([Y, neg(Negand), _], ValidatedSoFar),
+    NegExpr = cont.
 
 % Double Negation Elimination (¬¬E)
+valid_line(_,_,[LineNr, DoubleNegExpr, negnegel(X)], ValidatedSoFar) :-
+    member([X, neg(neg(Expr)), _], ValidatedSoFar),
+    DoubleNegExpr = Expr.
 
-% Ex Falso (From contradiction, derive any formula)
+% Double Negation Introduction
+valid_line(_,_, [LineNr, Expr, negnegint(X)], ValidatedSoFar) :-
+    member([X, Negand, _], ValidatedSoFar),
+    Expr = neg(neg(Negand)).
+
+% Contradiction Elimination (⊥e)
+valid_line(_,_, [LineNr, Expr, contel(X)], ValidatedSoFar) :-
+    member([X, cont, _], ValidatedSoFar).
+
+% Modus Tollens (MT)
+valid_line(_,_,[LineNr, MtExpr, mt(X, Y)], ValidatedSoFar) :-
+    member([X, imp(Antecedent, Conclusion), _], ValidatedSoFar),
+    member([Y, neg(Conclusion), _], ValidatedSoFar),
+    MtExpr = neg(Antecedent).
+
+% PBC rule -> need box
+
+% Law of the Excluded Middle (LEM)
+valid_line(_,_,[LineNr, or(LemExpr, neg(LemExpr)), lem], ValidatedSoFar).
 
 % verify -> read file
 verify(InputFileName) :- 
