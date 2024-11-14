fb1fcf0fc5e1402db9060dac5fe809dea5819050
glass123banan <glass123banan@gmail.com>
2024-11-14 11:13:39 +0100

    blä

--- DD1351-Logik-Lab2/beviskontroll.pl
+++ DD1351-Logik-Lab2/beviskontroll.pl
@@ -3,15 +3,11 @@
 valid_line(Prems, _, [LineNr, Expr, Premise], _) :-
     member(Expr, Prems),
     write('line prems: '),
-    write(LineNr),
-    write('\n'). 
+    write(LineNr), nl.
 
 % check if line belongs to goal 
-valid_line(_, Goal, [LineNr, Expr, _], _) :-
-    member(Expr, Goal),
-    write('linenr goal: '),
-    write(LineNr),
-    write('\n').
+valid_line(_, Goal, [_, Expr, _], _) :-
+    Expr = Goal.
 
 % valid_proof calls on helper func w validated steps
 valid_proof(Prems, Goal, ProofSteps) :-
@@ -19,9 +15,10 @@ valid_proof(Prems, Goal, ProofSteps) :-
 
 % basfall
 valid_proof_validator(_, Goal, [], ValidatedSoFar) :-
-    write(Goal),
-    write(ValidatedSoFar),
-    member(Goal, ValidatedSoFar).
+    write('hello'), nl,
+    member([_,Goal,_], ValidatedSoFar), 
+    write('basfall 2'), nl.
+
 
 % helper func that stores checked lines in ValidatedSoFar
 valid_proof_validator(Prems, Goal, [Step | RestOfProof], ValidatedSoFar) :-
@@ -38,15 +35,14 @@ or(P, Q).    % p v q
 
 % Implication Elimination (Modus Ponens, →E)
 valid_line(_, _, [LineNr, ImpelExpr, impel(X, Y)], ValidatedSoFar) :-
-    write('1'),
-    nth1(X, ValidatedSoFar, [X, Antecedent, _]),
-    write('2'),
-    nth1(Y, ValidatedSoFar, [Y, imp(Antecedent, Conclusion), _]),
-    write('3'),
+    % write('1'),
+    member([X, Antecedent, _], ValidatedSoFar),
+    % write('2'),
+    member([Y, imp(Antecedent, Conclusion), _], ValidatedSoFar),
+    % write('3'),
     Conclusion = ImpelExpr,
-    write('impen line: '),
-    write(LineNr),
-    write('\n').
+    write('impel line: '),
+    write(LineNr), nl.
 
 % valid proof check recursive case
 
@@ -77,10 +73,7 @@ verify(InputFileName) :-
     read(Goal),             % read goals
     read(Proof),            % read proof steps
     seen,                   % close file
-    write(Prems),
-    write('\n'),
-    write(Goal),
-    write('\n'),
-    write(Proof),
-    write('\n'),
-    ( valid_proof(Prems, Goal, Proof) -> write('yes') ; write('no') ).
+    write(Prems), nl,
+    write(Goal), nl,
+    write(Proof), nl, nl,
+    ( valid_proof(Prems, Goal, Proof) -> nl, write('yes'), nl ; nl, write('no') ), nl.
