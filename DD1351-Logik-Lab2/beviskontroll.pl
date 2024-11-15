8ad2736116c4dd42e0d7b9d8260e9c788ea8b775
glass123banan <glass123banan@gmail.com>
2024-11-15 15:43:28 +0100

    adding boxstuff

--- DD1351-Logik-Lab2/beviskontroll.pl
+++ DD1351-Logik-Lab2/beviskontroll.pl
@@ -10,12 +10,40 @@ valid_proof(Prems, Goal, ProofSteps) :-
 
 % basecase: check if Goal is present in ValidatedSoFar
 valid_proof_validator(_, Goal, [], ValidatedSoFar) :-
+    write("Validated so far (base): "), write(ValidatedSoFar), nl, 
     member([_,Goal,_], ValidatedSoFar).
 
-% helper func that stores checked lines in ValidatedSoFar
+% box recursive case: checks if its a box -> handles boxes
+valid_proof_validator(Prems, Goal, [Box | RestOfProof], ValidatedSoFar) :-
+    is_box(Box, ValidatedSoFar),       
+    write("Box: "), write(Box), nl, 
+    write("Rest of proof: "), write(RestOfProof), nl,
+    valid_proof_validator(Prems, Goal, RestOfProof, [Box | ValidatedSoFar]).  
+
+% recursive case: helper func that stores checked lines in ValidatedSoFar -> handles regular lines
 valid_proof_validator(Prems, Goal, [Step | RestOfProof], ValidatedSoFar) :-
+    write("normal line check: "), write(Step), nl,
     valid_line(Prems, Goal, Step, ValidatedSoFar), 
     valid_proof_validator(Prems, Goal, RestOfProof, [Step | ValidatedSoFar]).
+
+
+% box control - If list in list and first step is assumption -> box
+is_box(BoxStep, ValidatedSoFar) :-
+    is_list(BoxStep),
+    BoxStep = [[_,_, assumption]| _],
+    check_boxes(BoxStep, ValidatedSoFar).
+
+% recursively checks steps inside a box
+check_boxes([], _).
+
+% If the step is a nested box, validate it recursively
+check_boxes([NestedBox | RestOfBox], ValidatedSoFar) :-
+    is_box(NestedBox, ValidatedSoFar),                                    
+    check_boxes(RestOfBox, ValidatedSoFar). 
+
+check_boxes([StepInBox|RestOfBoxproof], ValidatedBoxProof) :-
+    valid_line(_,_,StepInBox, ValidatedBoxProof),
+    check_boxes(RestOfBoxproof, ValidatedBoxProof). 
 
 % Check each individual line if its valid
 % Format per proof line: [LineNr, Expr, Rule]
@@ -23,7 +51,20 @@ valid_proof_validator(Prems, Goal, [Step | RestOfProof], ValidatedSoFar) :-
 valid_line(Prems, _, [LineNr, Expr, Premise], _) :-
     member(Expr, Prems).
 
+% copy rule
+valid_line(_,_, [LineNr, CopyExpr, copy(X)], ValidatedSoFar) :-
+    member([X, CopiedExpr, _], ValidatedSoFar),
+    CopyExpr = CopiedExpr.
+
 % Implication Introduction (→I) -> need box
+% [Box | _] from ValidatedSoFar (in valid_proof_validator)
+valid_line(_,_, [LineNr, imp(Antecedent, Conclusion), impint(X,Y)], [Box | _]) :-
+    write("starting impint..."), nl,
+    member([X, Antecedent, assumption], Box), fail,
+    write("check antecedent: "), write(Antecedent), nl, 
+    member([Y, Conclusion, _], Box), !,
+    write("check conclusion: "), write(Conclusion), nl.
+    
 
 % Implication Elimination (Modus Ponens, →E)
 valid_line(_, _, [LineNr, ImpelExpr, impel(X, Y)], ValidatedSoFar) :-
@@ -53,7 +94,7 @@ valid_line(_,_,[LineNr, OrExpr, orint1(X)], ValidatedSoFar) :-
     OrExpr = or(Disjunct1, Disjunct2).
 
 % Disjunction Introduction Right (∨I₂)
-valid_line(_,_,[LineNr, OrExpr, orint1(X)], ValidatedSoFar) :-
+valid_line(_,_,[LineNr, OrExpr, orint2(X)], ValidatedSoFar) :-
     member([X, Disjunct2, _], ValidatedSoFar),
     OrExpr = or(Disjunct1, Disjunct2).
 
