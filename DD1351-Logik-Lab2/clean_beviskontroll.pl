8fac6b7fb3c32f8a1ec3ac35cac86b6e93b7e9ba
ilmal <nils@u1.se>
2024-11-16 18:02:53 +0100

    adding stuff

--- DD1351-Logik-Lab2/clean_beviskontroll.pl
+++ DD1351-Logik-Lab2/clean_beviskontroll.pl
@@ -15,14 +15,11 @@ valid_proof_validator(_, Goal, [], ValidatedSoFar) :-
 % box recursive case: checks if its a box -> handles boxes
 valid_proof_validator(Prems, Goal, [Box | RestOfProof], ValidatedSoFar) :-
     is_box(Box, ValidatedSoFar),       
-    write("Box: "), write(Box), nl, 
-    write("Rest of proof: "), write(RestOfProof), nl,
     !,      % if find box, dont go to recursive case
     valid_proof_validator(Prems, Goal, RestOfProof, [Box | ValidatedSoFar]).  
 
 % recursive case: helper func that stores checked lines in ValidatedSoFar -> handles regular lines
 valid_proof_validator(Prems, Goal, [Step | RestOfProof], ValidatedSoFar) :-
-    write("normal line check: "), write(Step), nl,
     valid_line(Prems, Goal, Step, ValidatedSoFar), 
     valid_proof_validator(Prems, Goal, RestOfProof, [Step | ValidatedSoFar]).
 
@@ -30,7 +27,6 @@ valid_proof_validator(Prems, Goal, [Step | RestOfProof], ValidatedSoFar) :-
 is_box(BoxStep, ValidatedSoFar) :-
     is_list(BoxStep),
     BoxStep = [[_,_, assumption]| _],
-    write("Found a box: "), write(BoxStep), nl,
     check_boxes(BoxStep, ValidatedSoFar).
 
 % recursively checks steps inside a box
@@ -39,13 +35,10 @@ check_boxes([], _).
 % If the step is a nested box, validate it recursively
 check_boxes([NestedBox | RestOfBox], ValidatedSoFar) :-
     is_box(NestedBox, ValidatedSoFar),                 
-    write("Checking nested box: "), write(NestedBox), nl,                   
     check_boxes(RestOfBox, ValidatedSoFar). 
 
 check_boxes([StepInBox|RestOfBoxproof], ValidatedBoxProof) :-
-    write("Checking step in box: "), write(StepInBox), nl,
     valid_line(_,_,StepInBox, ValidatedBoxProof),
-    write("Step in box validated: "), nl,
     check_boxes(RestOfBoxproof, ValidatedBoxProof). 
 
 % Check each individual line if its valid
@@ -56,149 +49,115 @@ valid_line(Prems, _, [LineNr, Expr, Premise], _) :-
     Premise = premise; 
     Premise = assumption,
     member(Expr, Prems),
-    !,
-    write("is part of premise"), nl.
+    !.
 
 % copy rule
 valid_line(_,_, [LineNr, CopyExpr, copy(X)], ValidatedSoFar) :-
     member([X, CopiedExpr, _], ValidatedSoFar),
     CopyExpr = CopiedExpr.
-    write("copy rule"), nl.
 
 % Implication Introduction (→I) -> need box
 % [Box | _] from ValidatedSoFar (in valid_proof_validator)
 valid_line(_,_, [LineNr, imp(Antecedent, Conclusion), impint(X,Y)], [Box | _]) :-
     member([X, Antecedent, assumption], Box), 
-    member([Y, Conclusion, _], Box),
-    write("impint"), nl.
+    member([Y, Conclusion, _], Box).
 
 % Implication Elimination (Modus Ponens, →E)
 valid_line(_, _, [LineNr, ImpelExpr, impel(X, Y)], ValidatedSoFar) :-
     member([X, Antecedent, _], ValidatedSoFar),                         % P is true
     member([Y, imp(Antecedent, Conclusion), _], ValidatedSoFar),        % P->Q is true
-    Conclusion = ImpelExpr,                                             % Q is thus true
-    write("impel"), nl.
+    Conclusion = ImpelExpr.                                             % Q is thus true
+
 
 % Conjunction Introduction (∧I)
 valid_line(_, _, [LineNr, AndExpr, andint(X, Y)], ValidatedSoFar) :-
     member([X, Conjunct1, _], ValidatedSoFar),
     member([Y, Conjunct2, _], ValidatedSoFar), 
-    AndExpr = and(Conjunct1, Conjunct2),
-    write("andint"), nl.
+    AndExpr = and(Conjunct1, Conjunct2).
 
 % Conjunction Elimination Left (∧E₁)
 valid_line(_,_,[LineNr, AndExpr, andel1(X)], ValidatedSoFar) :-
     member([X, and(Conjunct1, Conjunct2), _], ValidatedSoFar),
-    AndExpr = Conjunct1,
-    write("andel1"), nl.
+    AndExpr = Conjunct1.
 
 % Conjunction Elimination Right (∧E₂)
 valid_line(_,_,[LineNr, AndExpr, andel2(X)], ValidatedSoFar) :-
     member([X, and(Conjunct1, Conjunct2), _], ValidatedSoFar),
-    AndExpr = Conjunct2,
-    write("andel2"), nl.
+    AndExpr = Conjunct2.
 
 % Disjunction Introduction Left (∨I₁)
 valid_line(_,_,[LineNr, OrExpr, orint1(X)], ValidatedSoFar) :-
     member([X, Disjunct1, _], ValidatedSoFar),
-    OrExpr = or(Disjunct1, Disjunct2),
-    write("orint1"), nl.
+    OrExpr = or(Disjunct1, Disjunct2).
 
 % Disjunction Introduction Right (∨I₂)
 valid_line(_,_,[LineNr, OrExpr, orint2(X)], ValidatedSoFar) :-
     member([X, Disjunct2, _], ValidatedSoFar),
-    OrExpr = or(Disjunct1, Disjunct2),
-    write("orint2"), nl.
+    OrExpr = or(Disjunct1, Disjunct2).
 
 % Helper predicate to find a box (subproof) by start and end line numbers
 find_box(ValidatedSoFar, StartLine, EndLine, Box) :-
-    % write("find_box : "), write(StartLine), write(EndLine), nl,
     member(Box, ValidatedSoFar),
 
     Box = [[StartLine, _, assumption] |_],
-    write("Checking for box: "), write(Box), nl,
 
-    write("Validated: "), write(ValidatedSoFar), nl, 
-    write("Box found in validated: "), write(Box), nl, 
-
-
-    member([EndLine, _, _], Box),
-    write("found box"), nl.
+    member([EndLine, _, _], Box).
 
 % Disjunction Elimination (∨E)
 valid_line(_, _, [LineNr, OrelExpr, orel(OrExprLine, StartFirst, EndFirst, StartSecond, EndSecond)], ValidatedSoFar) :-
     OrExpr = or(LeftDisjunct, RightDisjunct),
-    write("Orexpr: "), write(OrExpr), nl, 
     member([OrExprLine, OrExpr, _], ValidatedSoFar),
 
-    write("find box1..."), nl,
     find_box(ValidatedSoFar, StartFirst, EndFirst, Box1), 
     Box1 = [[StartFirst, LeftDisjunct, assumption] | _],
     member([EndFirst, OrelExpr, _], Box1),
-    write("BOx 1: "), write(Box1), nl,
     
-    write("find box2..."), nl,
     find_box(ValidatedSoFar, StartSecond, EndSecond, Box2),
     Box2 = [[StartSecond, RightDisjunct, assumption] | _],
-    member([EndSecond, OrelExpr, _], Box2),
-    write("BOx 2: "), write(Box2), nl,
-
-    write("orel"), nl.
+    member([EndSecond, OrelExpr, _], Box2).
 
 % Negation Introduction (¬I) -> need box
 valid_line(_,_,[LineNr, neg(Negand), negint(X, Y)], [Box| _]) :-
     member([X, Negand, assumption], Box),
-    member([Y, cont, _], Box),
-    write("negint"), nl. 
+    member([Y, cont, _], Box).
 
 % Negation Elimination (¬E)
 valid_line(_,_,[LineNr, NegExpr, negel(X, Y)], ValidatedSoFar) :-
     member([X, Negand, _], ValidatedSoFar),
     member([Y, neg(Negand), _], ValidatedSoFar),
-    NegExpr = cont,
-    write("negel"), nl.
+    NegExpr = cont.
 
 % Double Negation Elimination (¬¬E)
 valid_line(_,_,[LineNr, DoubleNegExpr, negnegel(X)], ValidatedSoFar) :-
     member([X, neg(neg(Expr)), _], ValidatedSoFar),
-    DoubleNegExpr = Expr,
-    write("negnegel"), nl.
+    DoubleNegExpr = Expr.
 
 % Double Negation Introduction
 valid_line(_,_, [LineNr, Expr, negnegint(X)], ValidatedSoFar) :-
     member([X, Negand, _], ValidatedSoFar),
-    Expr = neg(neg(Negand)),
-    write("negnegint"), nl.
+    Expr = neg(neg(Negand)).
 
 % Contradiction Elimination (⊥e)
 valid_line(_,_, [LineNr, Expr, contel(X)], ValidatedSoFar) :-
-    member([X, cont, _], ValidatedSoFar),
-    write("contel"), nl.
+    member([X, cont, _], ValidatedSoFar).
 
 % Modus Tollens (MT)
 valid_line(_,_,[LineNr, MtExpr, mt(X, Y)], ValidatedSoFar) :-
     member([X, imp(Antecedent, Conclusion), _], ValidatedSoFar),
     member([Y, neg(Conclusion), _], ValidatedSoFar),
-    MtExpr = neg(Antecedent),
-    write("mt"), nl.
+    MtExpr = neg(Antecedent).
 
 % PBC rule -> need box
 valid_line(_,_, [LineNr, Expr, pbc(X, Y)], [Box |_]) :-
     member([X, neg(Expr), assumption], Box), 
-    member([Y, cont, _], Box),
-    write("pbc"), nl.
+    member([Y, cont, _], Box).
 
 % Law of the Excluded Middle (LEM)
-valid_line(_,_,[LineNr, or(LemExpr, neg(LemExpr)), lem], ValidatedSoFar),
-    write("lem"), nl.
+valid_line(_,_,[LineNr, or(LemExpr, neg(LemExpr)), lem], ValidatedSoFar).
 
 % verify -> read file
 verify(InputFileName) :- 
     see(InputFileName),     % open file with InputFileName
     read(Prems), read(Goal), read(Proof),            
     seen,                   % close file
-    write(Prems), nl,
-    write(Goal), nl,
-    write(Proof), nl, nl,
-    nl,
-    ( valid_proof(Prems, Goal, Proof) -> nl, write('yes'), nl ; nl, write('no') ), nl.
+    valid_proof(Prems, Goal, Proof).
